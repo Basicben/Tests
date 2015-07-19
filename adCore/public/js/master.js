7b1662903b9fee6864be6ce0d411f73d693e20f1
Basicben <benari_kutai@yahoo.com>
2015-07-19 17:36:29 +0300

    S

---
+++ adCore/public/js/master.js
@@ -0,0 +1,7 @@
+var app = angular.module('testApp',[]);
+
+app.controller('masterCntrl', function($scope) {
+
+	$scope.test = "TESTING 1 2 3";
+
+});
---
+++ adCore/server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use('/',express.static('./public')).listen(process.env.PORT || 3000);
+
+console.log('listenning on server...');
